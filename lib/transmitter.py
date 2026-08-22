f59d8ee074df9a769a71314905a773a6d1e78f7f
Adam Fistler <fistlerap@gmail.comL>
2026-08-22 18:55:50 -0400

    Added transmitter

---
+++ lib/transmitter.py
@@ -0,0 +1,13 @@
+from .context import Context
+from .transport import Transport
+
+
+class Transmitter:
+  def __init__(self, transport: Transport):
+    self.transport = transport
+
+  def transmit(self, context: Context):
+    if not isinstance(context, Context):
+      raise TypeError("context must be a Context instance")
+
+    return self.transport.send(context)
