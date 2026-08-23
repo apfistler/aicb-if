72985cd35ff822cbcbd1bfd6c6a3c9be06a2d2aa
Adam Fistler <fistlerap@gmail.comL>
2026-08-22 21:56:14 -0400

    Update transmit.py

--- lib/transmit.py
+++ lib/transmit.py
@@ -1,26 +1,26 @@
+import json
 from datetime import datetime
 from uuid import uuid4
 
-from .context import Context
-from .transport import Transport
-
 
 class Transmitter:
-  def __init__(self, transport: Transport):
+  def __init__(self, transport):
     self.transport = transport
 
-  def transmit(self, context: Context):
-    if not isinstance(context, Context):
-      raise TypeError("context must be a Context instance")
-
+  def transmit(self, message):
     filename = (
-      f"context-"
+      f"{message.type}-"
       f"{datetime.now().strftime('%Y%m%d-%H%M%S')}-"
-      f"{uuid4().hex[:8]}.txt"
+      f"{uuid4().hex[:8]}.json"
+    )
+
+    data = json.dumps(
+      message.to_dict(),
+      indent=2
     )
 
     self.transport.send(
-      context,
+      data,
       filename
     )
 
